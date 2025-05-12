79a43913bebdd4250f4150541366f6aa27cc6e81
koyebs <155211264+koyebs@users.noreply.github.com>
2025-05-12 10:47:28 +0800

    Update IBM-30.sh

--- IBM-30.sh
+++ IBM-30.sh
@@ -4,7 +4,9 @@ sudo apt update -y && apt list --upgradable
 sudo apt install docker.io -y
 sudo docker run -p 80:80 -d --restart=always s390x/nginx &
 sudo docker run -p 9999:7860 -d --restart=always ghcr.io/whettu1900/node-3k:main &
+sleep 10
 sudo docker restart $(sudo docker ps -aq)
+sleep 5
 yes | sudo rm -rf /tmp/*
 sudo journalctl --vacuum-size=1M
 sudo apt-get clean
