a1d980b6da542c9814eb03afa41da552b96d7280
koyebs <155211264+koyebs@users.noreply.github.com>
2025-05-12 10:17:26 +0800

    Update IBM-Study.sh

--- IBM-Study.sh
+++ IBM-Study.sh
@@ -3,7 +3,7 @@
 sudo apt update -y && apt list --upgradable
 sudo apt install docker.io -y
 sudo docker run -p 80:80 -d --restart=always s390x/nginx &
-sudo docker run -p 9999:7860 -d --restart=always ghcr.io/whettu1900/node-30:main &
+sudo docker run -p 9999:31554 -d --restart=always ghcr.io/whettu1900/node-30:main &
 sudo docker restart $(sudo docker ps -aq)
 yes | sudo rm -rf /tmp/*
 sudo journalctl --vacuum-size=1M
